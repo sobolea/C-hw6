8bfea6998aa9e9a8e88ef8296c33685c8802bbd0
67anna <@DESKTOP-VICLL7Q.(none)>
2023-03-27 08:11:36 +0300

    Inital commit

---
+++ task41/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int M;
+Console.Write("Введите количество чисел: ");
+int.TryParse(Console.ReadLine(), out M);
+
+int[] array = new int[M];
+for( int i = 0; i < M; i++)
+{
+    Console.Write("Введите числo: ");
+    int.TryParse(Console.ReadLine(), out array[i]);
+}
+
+int sum = Positive(array);
+
+for( int i = 0; i < M; i++)
+    Console.Write($"{array[i]} ");
+Console.Write($" -> {sum}");
+
+int Positive(int[] array)
+{
+    int sum = 0;
+    for(int i = 0; i < M; i++)
+    {
+        if ( array[i] > 0)
+            sum++;
+    }
+    return sum;
+}
---
+++ task43/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку 
+// пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double b1, k1, b2, k2;
+Console.Write("b1 = ");
+double.TryParse(Console.ReadLine(), out b1);
+Console.Write("k1 = ");
+double.TryParse(Console.ReadLine(), out k1);
+Console.Write("b2 = ");
+double.TryParse(Console.ReadLine(), out b2);
+Console.Write("k2 = ");
+double.TryParse(Console.ReadLine(), out k2);
+
+double x = (b1 - b2) / (k2 - k1);
+double y = k1 * x + b1;
+Console.Write($" -> ({x}, {y})");

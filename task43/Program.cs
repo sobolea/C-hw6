e7fbfd2b6c3274b7a15d320b660fb8a080ea9a87
sobolea <126894663+sobolea@users.noreply.github.com>
2023-04-08 20:48:28 +0300

    Update Program.cs

--- task43/Program.cs
+++ task43/Program.cs
@@ -14,6 +14,14 @@ double.TryParse(Console.ReadLine(), out b2);
 Console.Write("k2 = ");
 double.TryParse(Console.ReadLine(), out k2);
 
+/* Console.Write("Введите значения для прямой А: ");
+a = Console.ReadLine().Split().Where(x => int.TryParse(x,out _)).Select(int.Parse).ToArray();
+Console.Write("Введите значения для прямой B: ");
+b = Console.ReadLine().Split().Where(x => int.TryParse(x,out _)).Select(int.Parse).ToArray();
+
+double x = (a[1] - b[1]) / (b[0] - a[0]);
+double y = a[0] * x + a[1];
+
 double x = (b1 - b2) / (k2 - k1);
 double y = k1 * x + b1;
 Console.Write($" -> ({x}, {y})");
